4d32752fc0db1d20361b11e71f08b374eae12c40
David L. Inman <davidlevere@gmail.com>
2019-09-08 00:10:43 -0400

    working out the gadt and kinds needed to create an AST

--- src/Ast.hs
+++ src/Ast.hs
@@ -1,11 +1,41 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
 
 module Ast 
      (
 
      ) where
 
+data Type =
+    TBool
+    | TNumber
+    | TString
+    | TGeneric 
+    | TStruct 
+    | TAbstract  
+    | TDict Type Type 
+    | TList Type
 
-data Expr a where 
+
+data Expr (a::Type) where 
+    TrueLit :: Expr TBool
+    FalseLit :: Expr TBool
+    NumberLit :: Double -> Expr TNumber 
+    StringLit :: String -> Expr TString
+    Symbol :: String -> Expr a
+    Dot :: Expr TStruct -> String -> Expr a
+    IndexKey :: Expr (TDict a b) -> Expr a -> Expr b
+    And :: Expr TBool -> Expr TBool -> Expr TBool
+    Or :: Expr TBool -> Expr TBool -> Expr TBool
+    Xor :: Expr TBool -> Expr TBool -> Expr TBool
+    Eq :: Expr TBool -> Expr TBool -> Expr TBool
+    Neq :: Expr TBool -> Expr TBool -> Expr TBool
+    Not :: Expr TBool -> Expr TBool
     
-    If :  
+  
+
+blarg :: Expr TNumber -> Double
+blarg x =
+    case x of
+        NumberLit y -> y
